192dae53bebc30869d7381456b623ef33b90e55c
Mina Khalil <Mina.Khalil@itworx.com>
2024-03-29 20:42:06 +0200

    add unit tests to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TxtFileLoader from './components/file-loader/TxtFileLoader';
 
 function App() {
   return (
-    <div className='App'>
+    <div data-testid='txt-file-loader' className='App'>
       <TxtFileLoader></TxtFileLoader>
     </div>
   );
---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+// App.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('App Component', () => {
+  test('renders TxtFileLoader component', () => {
+    const { getByTestId } = render(<App />);
+    const txtFileLoaderElement = getByTestId('txt-file-loader');
+    expect(txtFileLoaderElement).toBeInTheDocument();
+  });
+});
